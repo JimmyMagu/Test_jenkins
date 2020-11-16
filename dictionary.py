325a59527eb023d49a4995a2cfd20f8de8c0413f
Jimmy Magu <m.gideon6@students.ku.ac.ke>
2020-11-16 10:51:55 +0300

    added a dictionary file

---
+++ dictionary.py
@@ -0,0 +1,16 @@
+words = set()
+
+
+def check(word):
+    if word in words:
+        return True
+    else:
+        return False
+
+
+def load(dictionary):
+    file = open(dictionary, "r")
+    for line in file:
+        words.add(line.rsplit())\
+    close(file)
+    return True
